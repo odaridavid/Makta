d22e448d84f3c5819486845b0b1125519fb68d22
Davidodari <davidkibzodari@gmail.com>
2019-01-25 00:12:24 +0300

    refactor : modify tests

--- app/src/androidTest/java/com/android/blackoder/makta/MainActivityTest.java
+++ app/src/androidTest/java/com/android/blackoder/makta/MainActivityTest.java
@@ -5,6 +5,7 @@ import android.support.test.runner.AndroidJUnit4;
 import android.view.WindowManager;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,8 +13,11 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 
 /**
@@ -38,18 +42,25 @@ public class MainActivityTest {
     @Test
     public void testBottomNavigationNavigatesCorrectly() {
 //        Check text on activity launch
-        onView(withId(R.id.message_text_view)).check(matches(withText(R.string.title_borrowed_books_view)));
+        checkTextView(R.string.title_borrowed_books_view);
 
 //        Navigate to Lent books view and check text view matches lent text
         onView(withId(R.id.navigation_lent)).perform(click());
-        onView(withId(R.id.message_text_view)).check(matches(withText(R.string.title_lent_books_view)));
+        checkTextView(R.string.title_lent_books_view);
 
 //        Navigate to Search view and check text view matches book search text
         onView(withId(R.id.navigation_search)).perform(click());
-        onView(withId(R.id.message_text_view)).check(matches(withText(R.string.title_book_search_view)));
+        checkTextView(R.string.title_book_search_view);
 
 //        Navigate to User View and  check text view matches profile text
         onView(withId(R.id.navigation_profile)).perform(click());
-        onView(withId(R.id.message_text_view)).check(matches(withText(R.string.title_user_profile_view)));
+        checkTextView(R.string.title_user_profile_view);
+    }
+
+    @Ignore
+    public void checkTextView(int textMatchResource) {
+        onView(withId(R.id.message_text_view))
+                .inRoot(withDecorView(is(mActivityActivityTestRule.getActivity().getWindow().getDecorView())))
+                .check(matches(withText(textMatchResource)));
     }
 }

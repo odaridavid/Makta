c612e768bb5a96ab49a655143962154a197356a2
Davidodari <davidkibzodari@gmail.com>
2019-02-24 19:42:46 +0300

    link borrowed and lent fragment

--- app/src/main/java/com/android/blackoder/makta/view/fragments/BorrowedFragment.java
+++ app/src/main/java/com/android/blackoder/makta/view/fragments/BorrowedFragment.java
@@ -1,8 +1,27 @@
 package com.android.blackoder.makta.view.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.blackoder.makta.R;
+
 /**
  * Created By blackcoder
  * On 30/01/19
  **/
-public final class BorrowedFragment {
+public final class BorrowedFragment extends Fragment {
+    public BorrowedFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_borrowed, container, false);
+    }
 }

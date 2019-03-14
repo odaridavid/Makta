d50dc9cc2429a776df33723cc3d17c6f180dd58b
Davidodari <davidkibzodari@gmail.com>
2019-03-14 21:05:40 +0300

    feat : Add Search Interface

--- app/src/main/java/com/android/blackoder/makta/view/fragments/SearchFragment.java
+++ app/src/main/java/com/android/blackoder/makta/view/fragments/SearchFragment.java
@@ -1,10 +1,27 @@
 package com.android.blackoder.makta.view.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.blackoder.makta.R;
 
 /**
  * Created By blackcoder
  * On 30/01/19
  **/
 public final class SearchFragment extends Fragment {
+    public SearchFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
 }

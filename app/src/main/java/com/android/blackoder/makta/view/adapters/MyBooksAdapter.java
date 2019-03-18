51481ef5f008f70f2b216a5c6322b109730a45e8
Davidodari <davidkibzodari@gmail.com>
2019-03-18 17:07:54 +0300

    feat :  Add recycler layout ,adapter and implementation

---
+++ app/src/main/java/com/android/blackoder/makta/view/adapters/MyBooksAdapter.java
@@ -0,0 +1,62 @@
+package com.android.blackoder.makta.view.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.blackoder.makta.R;
+import com.android.blackoder.makta.model.entities.Book;
+
+import java.util.List;
+
+/**
+ * Created By blackcoder
+ * On 18/03/19
+ **/
+public final class MyBooksAdapter extends RecyclerView.Adapter<MyBooksAdapter.BooksViewHolder> {
+
+    private List<Book> mBookList;
+
+    void setBookList(List<Book> bookList) {
+        mBookList = bookList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public BooksViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        int layout_id = R.layout.list_item_my_books;
+        View view = inflater.inflate(layout_id, viewGroup, false);
+        return new BooksViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BooksViewHolder booksViewHolder, int i) {
+        if (mBookList != null) {
+            Book currentBook = mBookList.get(i);
+            booksViewHolder.tvBookTitle.setText(currentBook.getTitle());
+            booksViewHolder.tvBookAuthor.setText(currentBook.getAuthor());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mBookList != null ? mBookList.size() : 0;
+    }
+
+    class BooksViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvBookTitle, tvBookAuthor;
+
+        BooksViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvBookTitle = itemView.findViewById(R.id.text_view_book_title);
+            tvBookAuthor = itemView.findViewById(R.id.text_view_book_author);
+        }
+    }
+
+}
--- app/src/main/java/com/android/blackoder/makta/view/profile/BookListActivity.java
+++ app/src/main/java/com/android/blackoder/makta/view/profile/BookListActivity.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
 import com.android.blackoder.makta.R;
 import com.android.blackoder.makta.model.entities.Book;
 import com.android.blackoder.makta.utils.AppUtils;
+import com.android.blackoder.makta.view.adapters.MyBooksAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,18 +20,30 @@ import java.util.List;
 public class BookListActivity extends AppCompatActivity {
 
     private List<Book> myBooks = new ArrayList<>();
+    private RecyclerView rvMyBooks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
-        RecyclerView rvMyBooks = findViewById(R.id.recycler_view_my_books);
+        rvMyBooks = findViewById(R.id.recycler_view_my_books);
         TextView tvNoBooks = findViewById(R.id.text_view_no_books);
         FloatingActionButton fabAddBook = findViewById(R.id.fab_add_book);
+        setupRecyclerView();
         fabAddBook.setOnClickListener(v -> {
             Intent intent = new Intent(BookListActivity.this, BookEntryActivity.class);
             startActivity(intent);
         });
         AppUtils.handleVisibility(myBooks, rvMyBooks, tvNoBooks);
+    }
+
+    private void setupRecyclerView() {
+        MyBooksAdapter lMyBooksAdapter = new MyBooksAdapter();
+        rvMyBooks.setAdapter(lMyBooksAdapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvMyBooks.setLayoutManager(linearLayoutManager);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(rvMyBooks.getContext(),
+                linearLayoutManager.getOrientation());
+        rvMyBooks.addItemDecoration(dividerItemDecoration);
     }
 }

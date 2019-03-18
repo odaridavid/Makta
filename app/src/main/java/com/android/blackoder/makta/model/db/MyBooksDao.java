401adf53513e95fb1159a70d0b47669bf5ab5c3d
Davidodari <davidkibzodari@gmail.com>
2019-03-18 13:20:17 +0300

    chore :  create dao interface for wishlist and personal books

---
+++ app/src/main/java/com/android/blackoder/makta/model/db/MyBooksDao.java
@@ -0,0 +1,11 @@
+package com.android.blackoder.makta.model.db;
+
+import android.arch.persistence.room.Dao;
+
+/**
+ * Created By blackcoder
+ * On 18/03/19
+ **/
+@Dao
+public interface MyBooksDao {
+}
